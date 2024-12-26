e6f1ab5af759dc40003c249c0a52ad943aa6a56b
AskatAsh <abdulaskat@gmail.com>
2024-12-26 21:28:36 +0600

    deployed server side code to vercel

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://tutor-books.web.app", "https://tutor-books.firebaseapp.com"]
   })
 );
 
